5acea6cb77d3fe8ebdd175eee8903ffdc40f36de
Anju Sebastian <christeenaanju@gmail.com>
2025-06-21 15:55:34 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -19,15 +19,21 @@ const allowedOrigins = [
   "https://chatapp-frontend-new.netlify.app",
 ];
 
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
